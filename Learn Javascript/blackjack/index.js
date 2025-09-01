b8ef0f82a387497695412c8e9c7c2f0925b21099
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2025-09-01 21:56:31 +0000

    Update index.js

--- Learn Javascript/blackjack/index.js
+++ Learn Javascript/blackjack/index.js
@@ -4,23 +4,38 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector(".card-el");
+
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards : " + firstCard + ", " + secondCard;
+  sumEl.textContent = "Sum : " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! Yo u've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
-
   // console.log(hasBlackJack);
   // console.log(isAlive);
   // console.log(logMessage);
   messageEl.textContent = message;
 }
-sumEl.textContent += sum;
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 12;
+  sum += card;
+  renderGame();
+
+  cardsEl.textContent =
+    "Cards : " + firstCard + ", " + secondCard + ", " + card;
+}
