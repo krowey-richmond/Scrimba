0d30e50fd6bc3b0ad9b1def3355a02e83265d564
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2025-09-20 13:13:50 +0000

    Add Chrome extension for leads tracker
    Introduces a new Chrome extension to track leads, including manifest, icon, and UI updates. Implements localStorage persistence, a delete-all button, and improved styling for the input, buttons, and list. The extension now allows users to save, view, and clear leads with a more user-friendly interface.

--- Learn Javascript/chrome-extension/index.js
+++ Learn Javascript/chrome-extension/index.js
@@ -1,23 +1,55 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  renderLeads();
-});
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+} else {
+  console.log("Leads array empty");
+}
 
-function renderLeads() {
+
+function render(lead) {
   let listItems = "";
-  for (i = 0; i < myLeads.length; i++) {
+  for (i = 0; i < lead.length; i++) {
     // const li = document.createElement("li");
     // li.textContent = myLeads[i];
     // ulEL.append(li);
 
-    listItems += "<li>" + myLeads[i] + "</li>";
-    console.log(listItems);
+    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    listItems += `
+    <li>
+          <a target='_blank' href='${lead[i]}'>
+          ${lead[i]}
+          </a>
+    </li>
+    `;
+    // console.log(listItems);
   }
 
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = [];
+  localStorage.clear();
+  render(myLeads);
+
+  console.log("All leads cleared!");
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  inputEl.value = " ";
+  render(myLeads);
+
+
+  // To verify that it works:
+  console.log(localStorage.getItem("myLeads"));
+});
+console.log(leadsFromLocalStorage);
